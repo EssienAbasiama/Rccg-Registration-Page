024e15da858d452d19f829468ffb2cb7fc8e0dfb
EssienAbasiama <essienabasiama11@gmail.com>
2023-07-29 20:43:45 -0800

    Updated Favicon

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -86,7 +86,6 @@ function Body() {
         </div>
         <form onSubmit={handleSubmit} className="form">
           {selectedImage && (
-            
             <div
               style={{
                 backgroundImage: `url(${selectedImage})`,
